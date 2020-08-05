f4ce59a88882a2c0b6070f8d1089814c02088275
Jochen Heckl <jochen.heckl@gmail.com>
2020-08-05 10:51:42 +0200

    Added compressed inspector display for validated ComponentPropertyBindingBuilders.

--- Editor/ComponentPropertyBindingBuilderEditor.cs
+++ Editor/ComponentPropertyBindingBuilderEditor.cs
@@ -1,4 +1,10 @@
-﻿using UnityEditor;
+﻿using System;
+using System.Linq;
+using System.Reflection;
+
+using UnityEditor;
+
+using UnityEngine;
 
 namespace de.JochenHeckl.Unity.DataBinding.Editor
 {
@@ -9,9 +15,35 @@ namespace de.JochenHeckl.Unity.DataBinding.Editor
         {
             serializedObject.Update();
 
-            DrawPropertiesExcluding( serializedObject, "m_Script" );
+            var componentPropertyBindingBuilder = serializedObject.targetObject as ComponentPropertyBindingBuilder;
+            var dataSourceType = componentPropertyBindingBuilder.gameObject.GetComponent<ViewBehaviour>().dataSourceType.Type;
+
+            var validationData = IsValidBindingBuilder( dataSourceType, componentPropertyBindingBuilder );
+
+            if ( validationData.isValid )
+            {
+                EditorGUILayout.LabelField( new GUIContent(
+                    $"<b>{componentPropertyBindingBuilder.sourcePath}</b> :" +
+                    $" <b><color=blue>{validationData.sourceProperty.PropertyType.Name}</color></b> is bound to " +
+                    $"<b>{componentPropertyBindingBuilder.targetComponent.GetType().Name}.{componentPropertyBindingBuilder.targetPath}</b>",
+                    $"Bound to the component {componentPropertyBindingBuilder.targetComponent} on game object {componentPropertyBindingBuilder.gameObject.name}." ),
+                    new GUIStyle(){ richText = true });
+            }
+            else
+            {
+                DrawPropertiesExcluding( serializedObject, "m_Script" );
+            }
 
             serializedObject.ApplyModifiedProperties();
         }
-    }
+
+		private (bool isValid, PropertyInfo sourceProperty) IsValidBindingBuilder( Type dataSourceType, ComponentPropertyBindingBuilder componentPropertyBindingBuilder )
+		{
+            var sourceProperty = Array.Find( dataSourceType.GetProperties(), x => x.Name == componentPropertyBindingBuilder.sourcePath );
+
+            var isValid = (sourceProperty != null);
+
+            return ( isValid, sourceProperty );
+		}
+	}
 }
