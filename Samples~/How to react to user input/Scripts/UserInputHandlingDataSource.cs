ff225c12312179a5390fc15b72ba480526216fa8
jochen <jochen.heckl@gmail.com>
2023-04-16 15:17:13 +0200

    Renames Sample ViewsAndDataSources
    to 'How to react to user input' and adds a binding for a button action.

---
+++ Samples~/How to react to user input/Scripts/UserInputHandlingDataSource.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+using static UnityEngine.UI.Dropdown;
+
+namespace de.JochenHeckl.Unity.DataBinding.Example
+{
+    public class UserInputHandlingDataSource
+        : DataSourceBase<UserInputHandlingDataSource>
+    {
+        public string TypedText { get; set; }
+        public string SelectedDropDownText { get; set; }
+        public string PressCounterText { get; set; }
+
+        public List<OptionData> DropDownOptions { get; set; }
+
+        public Action<int> HandleDropDownSelectionChanged { get; set; }
+        public Action HandleButtonPressed { get; set; }
+        public Action<string> HandleTypedTextChanged { get; set; }
+    }
+}
---
+++ Samples~/How to react to user input/Scripts/UserInputHandlingSetup.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace de.JochenHeckl.Unity.DataBinding.Example
+{
+    public class UserInputHandlingSetup : MonoBehaviour
+    {
+        public View view;
+
+        private UserInputHandlingDataSource viewModel;
+        private int pressCounter = 0;
+
+        public void Start()
+        {
+            var dropdownOptions = new List<Dropdown.OptionData>()
+            {
+                new Dropdown.OptionData() { text = "One Option" },
+                new Dropdown.OptionData() { text = "Other Option" },
+            };
+
+            viewModel = new UserInputHandlingDataSource();
+
+            viewModel.TypedText = "No Text was typed.";
+            viewModel.SelectedDropDownText = dropdownOptions[0].text;
+            viewModel.PressCounterText = MakePressCounterText(pressCounter);
+
+            viewModel.DropDownOptions = dropdownOptions;
+            viewModel.HandleTypedTextChanged = HandleTypedTextChanged;
+            viewModel.HandleDropDownSelectionChanged = HandleDropDownSelectionChanged;
+            viewModel.HandleButtonPressed = HandleButtonPressed;
+
+            view.DataSource = viewModel;
+        }
+
+        private void HandleDropDownSelectionChanged(int selectedOptionIndex)
+        {
+            viewModel.NotifyChanges(
+                (x) => x.SelectedDropDownText = x.DropDownOptions[selectedOptionIndex].text
+            );
+        }
+
+        private void HandleTypedTextChanged(string newText)
+        {
+            viewModel.NotifyChanges((x) => x.TypedText = newText);
+        }
+
+        private void HandleButtonPressed()
+        {
+            pressCounter++;
+            viewModel.NotifyChanges((x) => x.PressCounterText = MakePressCounterText(pressCounter));
+        }
+
+        private string MakePressCounterText(int pressCounter)
+        {
+            return $"You pressed a button {pressCounter} times";
+        }
+    }
+}
