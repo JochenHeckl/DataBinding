4c38df89699c2de8ef2fdc21a905695e2912ea68
jochen <jochen.heckl@gmail.com>
2024-01-27 14:12:54 +0100

    Disables unused menu

--- Editor/Scripts/DatabindingEditorMenu.cs
+++ Editor/Scripts/DatabindingEditorMenu.cs
@@ -4,10 +4,10 @@ namespace de.JochenHeckl.Unity.DataBinding.Editor
 {
     public static class DataBindingEditorMenu
     {
-        [MenuItem("Data Binding/About")]
-        public static void About()
-        {
-            EditorUtility.DisplayDialog("Data Binding", "Thank you for using DataBinding.", "OK");
-        }
+        // [MenuItem("Data Binding/About")]
+        // public static void About()
+        // {
+        //     EditorUtility.DisplayDialog("Data Binding", "Thank you for using DataBinding.", "OK");
+        // }
     }
 }
