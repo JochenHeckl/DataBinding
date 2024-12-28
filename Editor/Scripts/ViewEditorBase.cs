ff65347f1522273cf1de84494bc8f3189392d143
Jochen Heckl <jochen.heckl@gmail.com>
2024-12-28 11:13:49 +0100

    Refactor acquisition of valid data sources into separate class

--- Editor/Scripts/ViewEditorBase.cs
+++ Editor/Scripts/ViewEditorBase.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -20,7 +20,7 @@ namespace JH.DataBinding.Editor
 
         public virtual void OnEnable()
         {
-            ValidDataSources ??= GetValidDataSourceTypes();
+            ValidDataSources ??= DataBindingCommonData.GetValidDataSourceTypes();
         }
 
         protected void InitDatabindingEditorRootElement()
