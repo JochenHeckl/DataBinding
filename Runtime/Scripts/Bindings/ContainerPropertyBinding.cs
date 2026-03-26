6b57d88ad44701154bd6816776d8ef08916d1b49
Jochen Heckl <jochen.heckl@gmail.com>
2026-03-26 13:56:36 +0100

    Fix Container Children are not removed if datasource is set to null

--- Runtime/Scripts/Bindings/ContainerPropertyBinding.cs
+++ Runtime/Scripts/Bindings/ContainerPropertyBinding.cs
@@ -6,119 +6,127 @@ using UnityEngine;
 
 namespace JH.DataBinding
 {
-    [Serializable]
-    public class ContainerPropertyBinding
+  [Serializable]
+  public class ContainerPropertyBinding
+  {
+    [SerializeField]
+    [EnumerableBindingSourcePath]
+    private string sourcePath;
+
+    [SerializeField]
+    private Transform targetContainer;
+
+    [SerializeField]
+    private View elementTemplate;
+
+    private object dataSource;
+
+    private MethodInfo[] dataSourcePropertyAccessors;
+
+    public object DataSource
     {
-        [SerializeField]
-        [EnumerableBindingSourcePath]
-        private string sourcePath;
-
-        [SerializeField]
-        private Transform targetContainer;
-
-        [SerializeField]
-        private View elementTemplate;
-
-        private object dataSource;
-
-        private MethodInfo[] dataSourcePropertyAccessors;
-
-        public object DataSource
-        {
-            get => dataSource;
-            set
-            {
-                dataSource = value;
-                BindSource();
-            }
-        }
-
-        public string SourcePath
-        {
-            get => sourcePath;
-            set
-            {
-                sourcePath = value;
-                BindSource();
-            }
-        }
-
-        public Transform TargetContainer
-        {
-            get => targetContainer;
-            set => targetContainer = value;
-        }
-
-        public View ElementTemplate
-        {
-            get => elementTemplate;
-            set => elementTemplate = value;
-        }
-
-        private void BindSource()
-        {
-            if (dataSource != null && !String.IsNullOrEmpty(sourcePath))
-            {
-                dataSourcePropertyAccessors = dataSource
-                    .ResolvePublicPropertyPath(SourcePath, PathResolveOperation.GetValue)
-                    .ToArray();
-            }
-            else
-            {
-                dataSourcePropertyAccessors = Array.Empty<MethodInfo>();
-            }
-        }
-
-        public void UpdateBinding()
-        {
-            if (
-                (dataSourcePropertyAccessors != null)
-                && (ElementTemplate != null)
-                && (TargetContainer != null)
-            )
-            {
-                if (dataSource is null)
-                {
-                    return;
-                }
-
-                var boundInstances = (
-                    (
-                        dataSourcePropertyAccessors.InvokeGetAccessChain(dataSource)
-                        as IEnumerable<INotifyDataSourceChanged>
-                    ) ?? Array.Empty<INotifyDataSourceChanged>()
-                ).ToArray();
-
-                AddMissingChildren(boundInstances.Length);
-                RemoveSuperfluousChildren(boundInstances.Length);
-
-                for (var childIndex = 0; childIndex < boundInstances.Length; childIndex++)
-                {
-                    // ReSharper disable once Unity.PerformanceCriticalCodeInvocation
-                    var view = TargetContainer.GetChild(childIndex).GetComponent<View>();
-                    view.DataSource = boundInstances[childIndex];
-                }
-            }
-        }
-
-        private void AddMissingChildren(int numberOfRequiredChildren)
-        {
-            while (TargetContainer.childCount < numberOfRequiredChildren)
-            {
-                UnityEngine.Object.Instantiate(ElementTemplate, TargetContainer);
-            }
-        }
-
-        private void RemoveSuperfluousChildren(int numberOfChildrenToKeep)
-        {
-            for (
-                var childIndex = numberOfChildrenToKeep;
-                childIndex < TargetContainer.childCount;
-                ++childIndex
-            )
-            {
-                UnityEngine.Object.Destroy(TargetContainer.GetChild(childIndex).gameObject);
-            }
-        }
+      get => dataSource;
+      set
+      {
+        dataSource = value;
+        BindSource();
+      }
     }
+
+    public string SourcePath
+    {
+      get => sourcePath;
+      set
+      {
+        sourcePath = value;
+        BindSource();
+      }
+    }
+
+    public Transform TargetContainer
+    {
+      get => targetContainer;
+      set => targetContainer = value;
+    }
+
+    public View ElementTemplate
+    {
+      get => elementTemplate;
+      set => elementTemplate = value;
+    }
+
+    private void BindSource()
+    {
+      if (dataSource != null && !String.IsNullOrEmpty(sourcePath))
+      {
+        dataSourcePropertyAccessors = dataSource
+          .ResolvePublicPropertyPath(SourcePath, PathResolveOperation.GetValue)
+          .ToArray();
+      }
+      else
+      {
+        dataSourcePropertyAccessors = Array.Empty<MethodInfo>();
+      }
+    }
+
+    public void UpdateBinding()
+    {
+      if (
+        (dataSourcePropertyAccessors != null)
+        && (ElementTemplate != null)
+        && (TargetContainer != null)
+      )
+      {
+        if (dataSource is null)
+        {
+          RemoveSuperfluousChildren(0);
+          return;
+        }
+
+        var boundInstances = (
+          (
+            dataSourcePropertyAccessors.InvokeGetAccessChain(dataSource)
+            as IEnumerable<INotifyDataSourceChanged>
+          ) ?? Array.Empty<INotifyDataSourceChanged>()
+        ).ToArray();
+
+        AddMissingChildren(boundInstances.Length);
+        RemoveSuperfluousChildren(boundInstances.Length);
+
+        for (var childIndex = 0; childIndex < boundInstances.Length; childIndex++)
+        {
+          // ReSharper disable once Unity.PerformanceCriticalCodeInvocation
+          var view = TargetContainer.GetChild(childIndex).GetComponent<View>();
+          view.DataSource = boundInstances[childIndex];
+        }
+      }
+    }
+
+    private void AddMissingChildren(int numberOfRequiredChildren)
+    {
+      while (TargetContainer.childCount < numberOfRequiredChildren)
+      {
+        UnityEngine.Object.Instantiate(ElementTemplate, TargetContainer);
+      }
+    }
+
+    private void RemoveSuperfluousChildren(int numberOfChildrenToKeep)
+    {
+      for (
+        var childIndex = numberOfChildrenToKeep;
+        childIndex < TargetContainer.childCount;
+        ++childIndex
+      )
+      {
+        var childView = TargetContainer.GetChild(childIndex).GetComponent<View>();
+
+        if (childView != null)
+        {
+          childView.DataSource = null;
+        }
+
+        UnityEngine.Object.Destroy(TargetContainer.GetChild(childIndex).gameObject);
+      }
+    }
+  }
 }
