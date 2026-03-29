491ce8728269a34a262558d18829ac5656fabf56
Jochen Heckl <jochen.heckl@gmail.com>
2026-03-29 11:09:45 +0200

    #49 update Container Binding Test scene to latest version

--- Samples~/Container Binding Test/Scripts/FastSpawnTestRunner.cs
+++ Samples~/Container Binding Test/Scripts/FastSpawnTestRunner.cs
@@ -3,39 +3,38 @@ using UnityEngine;
 
 namespace JH.DataBinding.Examples.ContainerBindingTest
 {
-    public class FastSpawnTestRunner : MonoBehaviour
+  public class FastSpawnTestRunner : MonoBehaviour
+  {
+    public View containerView;
+
+    [SerializeField]
+    private int count = 10;
+
+    [SerializeField]
+    private float maxDistance = 5f;
+
+    [ContextMenu(nameof(Start))]
+    private void Start()
     {
-        public View containerView;
+      containerView.DataSource = new ContainerViewDataSource()
+      {
+        Items = Enumerable.Range(0, count).Select(_ => MakeRandomItem()).ToArray(),
+      };
+    }
 
-        [SerializeField]
-        private int count = 10;
-
-        [SerializeField]
-        private float maxDistance = 5f;
-
-        [ContextMenu(nameof(Start))]
-        private void Start()
-        {
-            containerView.DataSource = new ContainerViewDataSource()
-            {
-                Items = Enumerable.Range(0, Random.Range(1, count))
-                .Select(_ => MakeRandomItem()).ToArray()
-            };
-            }
-
-        private ItemDataSource MakeRandomItem()
+    private ItemDataSource MakeRandomItem()
     {
       return new ItemDataSource
       {
         Position = Random.insideUnitSphere * maxDistance,
         Rotation = Random.rotation,
         Scale = new Vector3(
-              Random.Range(0.5f, 2.0f),
-              Random.Range(0.5f, 2.0f),
-              Random.Range(0.5f, 2.0f)
-          ),
-        Color = Random.ColorHSV(0.2f, 0.8f)
+          Random.Range(0.5f, 2.0f),
+          Random.Range(0.5f, 2.0f),
+          Random.Range(0.5f, 2.0f)
+        ),
+        Color = Random.ColorHSV(0.2f, 0.8f),
       };
     }
-    }
+  }
 }
